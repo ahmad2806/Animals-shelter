3222849f492b79c35a693d35f88165dd5a3bf0dc
montaserja <montaser_12@hotmail.com>
2018-04-16 19:54:45 +0300

    login-eddited

--- AS-app/src/app/app.module.ts
+++ AS-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
+import {FormsModule} from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +15,10 @@ import { NavBarComponent } from './header/nav-bar/nav-bar.component';
 import { CreateVolComponent } from './volunteer/create-vol/create-vol.component';
 import { VolListComponent } from './volunteer/vol-list/vol-list.component';
 import { VolNavbarComponent } from './volunteer/vol-navbar/vol-navbar.component';
-
+const appRoutes:Routes=[
+  {path:'volenteer',component:VolunteerComponent},
+  {path:'',component:LoginComponent},
+];
 
 @NgModule({
   declarations: [
@@ -30,7 +35,9 @@ import { VolNavbarComponent } from './volunteer/vol-navbar/vol-navbar.component'
     VolNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
