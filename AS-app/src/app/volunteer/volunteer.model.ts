888161f37863cc89136e8fc3833aea599b193d6c
ahmad2806 <ahmadhashem2806@gmail.com>
2018-04-24 12:51:59 +0300

    merge

--- AS-app/src/app/volunteer/volunteer.model.ts
+++ AS-app/src/app/volunteer/volunteer.model.ts
@@ -1,9 +1,15 @@
-export class volunteer{
-    constructor(public Name:string,public familyName:string,public ID:number,
-    public address:string,public gender:string,public date:Date,public mail:string
-,public tel:number,public tel1:number,public tel2:number,public type:string,public career:string,public car:string,public incar:string
-,public sunday:boolean,public monday:boolean,public tuesday:boolean,public wednesday:boolean,public thursday:boolean
-,public friday:boolean){
-        
+import { EmailValidator } from "@angular/forms";
+import { FreeDayes } from "./free-days.model";
+
+export class VolunteerModel{
+    constructor(public name:string, public id: string, 
+                public birthday: Date, public address: string, 
+                public phone: string, public telePhone: string, 
+                public homePhone: string, 
+                public email: EmailValidator, 
+                public volunteerType: string, 
+                public freeDays: FreeDayes, public hasCar: boolean, 
+                public agreeToLeft: boolean, public job: string){
+
     }
 }
---
+++ AS-app/src/app/volunteer/volunteers.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { VolunteerModel } from './volunteer.model';
+import { FreeDayes } from './free-days.model';
+import { EmailValidator } from '@angular/forms';
+
+@Injectable()
+export class VolunteersService {
+  volunteers:VolunteerModel[]=[
+    new VolunteerModel("אחמד","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("מונתצר","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("מוחמד","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("עומר","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+    new VolunteerModel("אבראהים","205417744",new Date,"beit-hanina","0524651749","",
+                        "025859294",new EmailValidator,"cat",new FreeDayes(true,false,true,true,true,false,false),
+                        true,false,"software engineering"),
+  ];
+  constructor() { 
+  }
+  public get Volunteers(){
+    return this.volunteers.slice;
+  }
+  
+   public add(vol:volunteer){
+    this.volunteers.push(vol);
+   }
+
+}
