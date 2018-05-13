5589132e271ecaf67de186482e08b83212ba47f0
omarburqan <omar.b__95@hotmail.com>
2018-05-13 20:31:08 +0300

    dd

--- AS-app/src/app/users/add-user/add-user.component.ts
+++ AS-app/src/app/users/add-user/add-user.component.ts
@@ -28,18 +28,20 @@ export class AddUserComponent {
     for (let index = 0; index < this.UserService.UsersList.length; index++) {
       if (this.username == this.UserService.UsersList[index].username) {
         alert("username is already in use");
+        return;
       }
     }
     if (Vper == false && Dper == false && Aper == false) {
       alert("please give at least 1 permission for the new user");
+      return;
     }
     if (this.password != this.password2) {
-      alert("confirm password isn't correct")
+      alert("confirm password isn't correct");
+      return;
     }
     else { //on Success
       newUser = new User(this.name, this.tel, this.email, this.username, this.password, Vper, Dper, Aper, false);
       this.UserService.usersList.push(newUser);
-      // this.router.navigate(["/Header/users"]);
     }
   }
 
