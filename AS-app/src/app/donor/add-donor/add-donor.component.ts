3a8012c1a8d622289f52e67c158e5125ac186c68
ibrahimidk <ibrahimidkedek@gmail.com>
2018-05-15 11:00:39 +0300

    add-donor

--- AS-app/src/app/donor/add-donor/add-donor.component.ts
+++ AS-app/src/app/donor/add-donor/add-donor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DonorService } from '../donor.service';
+import { NgForm, FormControl } from '@angular/forms';
+import { DonorModel } from '../donor.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-donor',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-donor.component.css']
 })
 export class AddDonorComponent implements OnInit {
+  @ViewChild('f') newDonorForm: NgForm;
+  name="";
+  id="";
+  address="";
+  phone="";
+  email="";
+  extraphone="";
+  birthday:Date;
+  amount="";
 
-  constructor() { }
+
+  Foundation=false;
+  private=false;
+  constructor(private donor:DonorService,private router: Router) { }
 
   ngOnInit() {
   }
 
+  radioChoice(choice:string){
+    if(choice=='Foundation'){
+      this.Foundation=true;
+      this.private=false;
+    }
+    if(choice=='private'){
+      this.private=true;
+      this.Foundation=false;
+    }
+  }
+  save(){
+    let newDonor:DonorModel;
+    if(this.Foundation==true){
+
+    }
+    if(this.private==true){
+      newDonor= new DonorModel(this.name,this.id,this.birthday,this.address,this.phone,this.extraphone,this.email,"פרטי",[]);
+      this.donor.donor.push(newDonor);
+      this.router.navigate(["/Header/donor/donorList"]);
+
+    }
+  }
 }
